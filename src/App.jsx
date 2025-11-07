2a360b46459118ed9dab9e8550d217dda59a80cc
Himanshu Raj Prakash <himanshu.prakash@mitwpu.edu.in>
2025-11-07 09:37:52 +0530

    Add login functionality to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import LoginPage from "./LoginPage";
 import UserDashboard from "./UserDashboard";
 
 export default function App() {
-  return <UserDashboard />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({ name: "", email: "" });
+
+  const handleLogin = (userData) => {
+    setUser(userData);
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <>
+      {!isLoggedIn ? (
+        <LoginPage onLogin={handleLogin} />
+      ) : (
+        <UserDashboard user={user} />
+      )}
+    </>
+  );
 }
